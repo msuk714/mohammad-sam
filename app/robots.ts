f58500e67eea3cbb2037633c7e2eebdad4c27d6d
msuk714 <iammsuk204@gmail.com>
2026-08-10 07:41:59 +0500

    Delete app directory

--- app/robots.ts
+++
@@ -1,7 +0,0 @@
-import type { MetadataRoute } from "next";
-import { getSiteUrl } from "@/data/url";
-
-export default function robots(): MetadataRoute.Robots {
-  const base = getSiteUrl();
-  return { rules: { userAgent: "*", allow: "/", disallow: ["/api/"] }, sitemap: `${base}/sitemap.xml` };
-}
